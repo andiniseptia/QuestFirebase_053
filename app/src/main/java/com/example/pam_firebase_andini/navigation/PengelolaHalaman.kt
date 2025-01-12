1b56e8d7bee053e9923a876dab3112c5dfaf9c09
andiniseptia <andiniseptiandri124@gmail.com>
2025-01-12 18:30:31 +0700

    Menambahkan fungsi PengelolaHalaman untuk navigasi

---
+++ app/src/main/java/com/example/pam_firebase_andini/navigation/PengelolaHalaman.kt
@@ -0,0 +1,18 @@
+package com.example.pam_firebase_andini.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pam_firebase_andini.ui.view.HomeScreen
+import com.example.pam_firebase_andini.ui.view.InsertMhsView
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+) {
+
+}
