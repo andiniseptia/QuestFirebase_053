5137173b31df4a43f1f8daf715b885894690bd72
andiniseptia <andiniseptiandri124@gmail.com>
2025-01-12 18:14:10 +0700

    Menambah data class untuk error

--- app/src/main/java/com/example/pam_firebase_andini/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pam_firebase_andini/ui/viewmodel/InsertViewModel.kt
@@ -11,6 +11,21 @@ import kotlinx.coroutines.launch
 import java.text.Normalizer.Form
 
 
+
+data class FormErrorState(
+    val nim: String? = null,
+    val nama: String? = null,
+    val jenis_kelamin: String? = null,
+    val alamat: String? = null,
+    val kelas: String? = null,
+    val angkatan: String? = null
+) {
+    fun isValid(): Boolean {
+        return nim == null && nama == null && jenis_kelamin == null &&
+                alamat == null && kelas == null && angkatan == null
+    }
+}
+
 data class MahasiswaEvent(
     val nim: String = "",
     val nama: String = "",
