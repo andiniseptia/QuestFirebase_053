e81516591909ab585f7e3b01b25f0403a5c17b59
andiniseptia <andiniseptiandri124@gmail.com>
2025-01-12 18:11:06 +0700

    Menambahkan fungsi delete di home view model

--- app/src/main/java/com/example/pam_firebase_andini/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/pam_firebase_andini/ui/viewmodel/HomeViewModel.kt
@@ -47,4 +47,14 @@ class HomeViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
                }
         }
     }
+
+    fun deleteMahasiswa(mahasiswa: Mahasiswa) {
+        viewModelScope.launch {
+            try {
+                mhs.deleteMahasiswa(mahasiswa)
+            } catch (e: Exception) {
+                mhsUIState = HomeUiState.Error(e)
+            }
+        }
+    }
 }
